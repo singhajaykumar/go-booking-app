81c8b5551cf66e6ffeef52a42cd11f74ba5c0ce5
Ajay Singh <aks.vsingh@gmail.com>
2025-02-16 22:48:41 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to Booking application of GO Conference")
+
+	var conferenceName string = "Go Conference"
+
+	fmt.Println(conferenceName)
+}
