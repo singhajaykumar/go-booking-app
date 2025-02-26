2e75604eee1a50e9fbc06c61eb8eb1081620ea8f
Ajay Singh <aks.vsingh@gmail.com>
2025-02-26 21:01:03 +0530

    added  Scan information

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ func main() {
 	fmt.Println("Welcome to Booking application of GO Conference")
 
 	var conferenceName string = "Go Conference"
+	var noTicket int
 
 	fmt.Println(conferenceName)
+
+	fmt.Println("Enter how many tickets you need ?")
+	fmt.Scan(&noTicket)
+
+	fmt.Printf("You booked ticket = %v", noTicket)
+
 }
